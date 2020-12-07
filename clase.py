daf38f60368ce8148c9f50bcd608cf88c256dc3b
jaba-840-g3 <ja@zdb.es>
2020-12-07 10:57:28 +0100

    prueba

---
+++ clase.py
@@ -0,0 +1,15 @@
+class conClase():
+    def visible(self):
+        print ("visible")
+        
+    def __oculto(self):
+        print("oculto")
+obj = conClase()
+obj.visible()
+
+try:
+    obj.__oculto()
+except:
+    print("fallido")
+    
+obj.conClase.__oculto()
